4dc9f72d41c93f38ca524c418be10361956dae6e
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-09-17 11:59:12 -0400

    added updated option

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -27,5 +28,24 @@ namespace Testing.Controllers
             var product = repo.GetProduct(id);
             return View(product);
         }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
+     
     }
 }
--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -8,5 +8,6 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        void UpdateProduct(Product product);
     }
 }
